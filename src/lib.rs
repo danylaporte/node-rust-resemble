9f7df39a7c7f9e14acc15c576045eb4fd539134e
Dany Laporte <dany_laporte@hotmail.com>
2020-05-26 08:58:26 -0400

    update rust dep

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,23 @@ extern crate image;
 extern crate libc;
 extern crate rust_resemble as resemble;
 
-use libc::{uint8_t, size_t};
-use image::load_from_memory;
+use image::{load_from_memory, ImageFormat};
+use libc::size_t;
 use std::mem::forget;
 
 #[no_mangle]
-pub extern "C" fn get_diff_image(img1: *const u8,
-                                 img1_length: size_t,
-                                 img2: *const u8,
-                                 img2_length: size_t,
-                                 diff_img: *mut *mut uint8_t)
-                                 -> usize {
-
+pub extern "C" fn get_diff_image(
+    img1: *const u8,
+    img1_length: size_t,
+    img2: *const u8,
+    img2_length: size_t,
+    diff_img: *mut *mut u8,
+) -> usize {
     let (v1, v2) = unsafe {
-        (std::slice::from_raw_parts(img1, img1_length),
-         std::slice::from_raw_parts(img2, img2_length))
+        (
+            std::slice::from_raw_parts(img1, img1_length),
+            std::slice::from_raw_parts(img2, img2_length),
+        )
     };
 
     let img1 = load_from_memory(v1).unwrap();
@@ -26,7 +28,7 @@ pub extern "C" fn get_diff_image(img1: *const u8,
     let r = resemble::compare_images(&img1, &img2, &opts);
     let mut out_buffer = Vec::new();
 
-    r.image.save(&mut out_buffer, image::PNG).unwrap();
+    r.image.write_to(&mut out_buffer, ImageFormat::Png).unwrap();
     out_buffer.shrink_to_fit();
 
     unsafe {
@@ -39,14 +41,17 @@ pub extern "C" fn get_diff_image(img1: *const u8,
 }
 
 #[no_mangle]
-pub extern "C" fn get_mismatch_percent(img1: *const u8,
-                                       img1_length: size_t,
-                                       img2: *const u8,
-                                       img2_length: size_t)
-                                       -> f64 {
+pub extern "C" fn get_mismatch_percent(
+    img1: *const u8,
+    img1_length: size_t,
+    img2: *const u8,
+    img2_length: size_t,
+) -> f64 {
     let (v1, v2) = unsafe {
-        (std::slice::from_raw_parts(img1, img1_length),
-         std::slice::from_raw_parts(img2, img2_length))
+        (
+            std::slice::from_raw_parts(img1, img1_length),
+            std::slice::from_raw_parts(img2, img2_length),
+        )
     };
 
     let img1 = load_from_memory(v1).unwrap();
